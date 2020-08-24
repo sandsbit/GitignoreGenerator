d3d221383092dd192c4f8d0922f900520cca5b84
Nikita Serba <nikitaserba@icloud.com>
2020-08-24 22:16:53 +0300

    Add gitignore template class

---
+++ src/main/java/me/nikitaserba/GitignoreGenerator/api/templates/GitignoreTemplate.java
@@ -0,0 +1,74 @@
+/*
+ * GitignoreGenerator
+ * https://github.com/sandsbit/GitignoreGenerator
+ *
+ * Copyright (C) 2020 Nikita Serba. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.nikitaserba.GitignoreGenerator.api.templates;
+
+import me.nikitaserba.GitignoreGenerator.api.sources.GitignoreSource;
+
+/**
+ * Class that stores information about .gitignore template:
+ * text, source, type e.g.
+ *
+ * This class is read-only
+ *
+ * This class is final to allow java use some optimizations. This
+ * tag will be removed if it's needed to inherit any class from
+ * this class.
+ *
+ * @version 1.0
+ */
+public final class GitignoreTemplate {
+
+    protected String name; // e.g. "Windows", "C/C++" or "JetBrains"
+    protected TemplateType type;
+    protected GitignoreSource source;
+    protected String sourceData;  // data source which was used GitignoreSource class (url, file path, etc.)
+
+    protected String gitignoreContent;
+
+    public GitignoreTemplate(String name, TemplateType type, GitignoreSource source,
+                             String sourceData, String gitignoreContent) {
+        this.name = name;
+        this.type = type;
+        this.source = source;
+        this.sourceData = sourceData;
+        this.gitignoreContent = gitignoreContent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TemplateType getType() {
+        return type;
+    }
+
+    public GitignoreSource getSource() {
+        return source;
+    }
+
+    public String getSourceData() {
+        return sourceData;
+    }
+
+    public String getContent() {
+        return gitignoreContent;
+    }
+}
---
+++ src/main/java/me/nikitaserba/GitignoreGenerator/api/templates/TemplateType.java
@@ -0,0 +1,35 @@
+/*
+ * GitignoreGenerator
+ * https://github.com/sandsbit/GitignoreGenerator
+ *
+ * Copyright (C) 2020 Nikita Serba. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.nikitaserba.GitignoreGenerator.api.templates;
+
+/**
+ * Possible .gitignore templates categories.
+ *
+ * @version 1.0
+ */
+public enum TemplateType {
+    OS,  // .gitignore used for projects that are developed in a certain OS (e.g. Windows or macOS)
+    PROGRAMMING_LANGUAGE,  // .gitignore for certain programming language
+    LIBRARY,  // .gitignore used with projects that use certain library (e.g. Qt)
+    UTILITY,  // .gitignore used when certain utility (e.g. compiler) is used in project
+    IDE,  // .gitignore used when projects is developed in certain IDE
+    OTHER
+}
